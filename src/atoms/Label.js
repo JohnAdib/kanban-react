c3f6f5c9fc1d24fe98a39a06a0ec8ebadd48a0e2
MrAdib <Mr.Javad.Adib@gmail.com>
2022-08-31 07:03:08 +0430

    add select and label to atom

---
+++ src/atoms/Label.js
@@ -0,0 +1,9 @@
+function Label(props) {
+  return (
+    <label htmlFor={props.htmlFor} className={props.className}>
+      {props.children}
+    </label>
+  );
+}
+
+export default Label;
---
+++ src/atoms/Select.js
@@ -0,0 +1,24 @@
+function Select(props) {
+  if (!props.options) {
+    return;
+  }
+
+  const options = props.options.map((x) => (
+    <option key={x} value={x}>
+      {x}
+    </option>
+  ));
+
+  return (
+    <select
+      id={props.id}
+      className={props.className}
+      onClick={props.onClick}
+      onChange={props.onChange}
+    >
+      {options}
+    </select>
+  );
+}
+
+export default Select;
