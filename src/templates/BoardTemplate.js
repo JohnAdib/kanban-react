a39507597d1478e614e8be4246814ccd00d00895
MrAdib <Mr.Javad.Adib@gmail.com>
2022-08-31 19:56:17 +0430

    improve template to use state hook instead of class

--- src/templates/BoardTemplate.js
+++ src/templates/BoardTemplate.js
@@ -1,58 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import BoardHeader from "./../organisms/BoardHeader";
 import BoardLists from "./../organisms/BoardLists";
 
-class BoardTemplate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      boardData: this.props.data,
-      inputAddNewList: "",
-      inputAddNewCard: ""
-    };
+function BoardTemplate(props) {
+  const [boardData, setBoardData] = useState(props.data);
+  const [inputAddNewList, setNewList] = useState("");
+  const [inputAddNewCard, setNewCard] = useState("");
 
-    this.handleBoardTitleChange      = this.handleBoardTitleChange.bind(this);
-    this.handleChangeInputAddNewList = this.handleChangeInputAddNewList.bind(this);
-    this.handleSubmitNewList         = this.handleSubmitNewList.bind(this);
-    this.handleChangeListTitle       = this.handleChangeListTitle.bind(this);
-    this.handleArchiveList           = this.handleArchiveList.bind(this);
+  updatePageTitle();
 
-    this.handleChangeInputAddNewCard = this.handleChangeInputAddNewCard.bind(this);
-    this.handleSubmitNewCard         = this.handleSubmitNewCard.bind(this);
-    this.handleChangeCard            = this.handleChangeCard.bind(this);
-    this.handleArchiveCard           = this.handleArchiveCard.bind(this);
-
-    this.updatePageTitle();
-  }
-
-  handleBoardTitleChange(event) {
-    const myData = { ...this.state.boardData };
+  function handleBoardTitleChange(event) {
+    const myData = { ...boardData };
     let newTitle = event.target.value;
     if (!newTitle) {
       newTitle = "Board Title";
     }
     myData.title = newTitle;
-    this.setState({ boardData: myData });
-    this.props.onBoardDataChange(myData);
+    setBoardData(myData);
+    props.onBoardDataChange(myData);
 
-    this.updatePageTitle(newTitle);
+    updatePageTitle(newTitle);
   }
 
-  updatePageTitle(newTitle) {
+  function updatePageTitle(newTitle) {
     // if newTitle passed, use it because of state delay updating
-    const myTitle = newTitle || this.state.boardData.title;
+    const myTitle = newTitle || boardData.title;
     // update page title
-    document.title = myTitle + " | " + this.state.boardData.brand;
+    document.title = myTitle + " | " + boardData.brand;
   }
 
-  handleChangeInputAddNewList(event) {
-    this.setState({ inputAddNewList: event.target.value });
+  function handleChangeInputAddNewList(event) {
+    setNewList(event.target.value);
   }
 
-  handleSubmitNewList(event) {
+  function handleSubmitNewList(event) {
     event.preventDefault();
-    const myData = { ...this.state.boardData };
-    const newTitle = this.state.inputAddNewList;
+    const myData = { ...boardData };
+    const newTitle = inputAddNewList;
     if (!newTitle) {
       return;
     }
@@ -62,12 +46,13 @@ class BoardTemplate extends React.Component {
       cards: []
     };
     myData.lists.push(newListArr);
-    this.setState({ boardData: myData, inputAddNewList: "" });
-    this.props.onBoardDataChange(myData);
+    setBoardData(myData);
+    setNewList("");
+    props.onBoardDataChange(myData);
   }
 
-  handleChangeListTitle(event) {
-    const myData = { ...this.state.boardData };
+  function handleChangeListTitle(event) {
+    const myData = { ...boardData };
     let newTitle = event.target.value;
     if (!newTitle) {
       newTitle = "List Title";
@@ -77,99 +62,87 @@ class BoardTemplate extends React.Component {
     // change title
     myData.lists[listIndex].title = newTitle;
 
-    this.setState({ boardData: myData });
-    this.props.onBoardDataChange(myData);
+    setBoardData(myData);
+    props.onBoardDataChange(myData);
   }
 
-  handleArchiveList(event) {
-    const myData = { ...this.state.boardData };
+  function handleArchiveList(event) {
+    const myData = { ...boardData };
     const listId = parseInt(event.target.closest("div").dataset.father);
     const listIndex = myData.lists.findIndex((el) => el.id === listId);
 
     // delete list
     myData.lists.splice(listIndex, 1);
 
-    this.setState({ boardData: myData });
-    this.props.onBoardDataChange(myData);
+    setBoardData(myData);
+    props.onBoardDataChange(myData);
   }
 
-  handleChangeInputAddNewCard(event) {
-    this.setState({ inputAddNewCard: event.target.value });
+  function handleChangeInputAddNewCard(event) {
+    setNewCard(event.target.value);
   }
 
-  handleSubmitNewCard(event) {
+  function handleSubmitNewCard(event) {
     event.preventDefault();
     // read parent id to add card
     const listId = parseInt(event.target.dataset.list);
-    const myData = { ...this.state.boardData };
+    const myData = { ...boardData };
     const listIndex = myData.lists.findIndex((el) => el.id === listId);
-    const newCardArr = this.validateCardTitle(
-      myData,
-      this.state.inputAddNewCard
-    );
-    if(!newCardArr)
-    {
+    const newCardArr = validateCardTitle(myData, inputAddNewCard);
+    if (!newCardArr) {
       return;
     }
 
     myData.lists[listIndex].cards.push(newCardArr);
     // clean input after add
-    this.setState({ boardData: myData, inputAddNewCard: "" });
+    setBoardData(myData);
+    setNewCard("");
     // save data inside storage
-    this.props.onBoardDataChange(myData);
+    props.onBoardDataChange(myData);
   }
 
-  handleChangeCard(event) {
-    const myData = { ...this.state.boardData };
+  function handleChangeCard(event) {
+    const myData = { ...boardData };
     let newVal = event.target.value;
     if (!newVal) {
-      newVal = "Board Title";
+      newVal = "#";
     }
     const listId = parseInt(event.target.dataset.grandfather);
     const cardId = parseInt(event.target.dataset.father);
 
-    const listIndex = parseInt(
-      myData.lists.findIndex((el) => el.id === listId)
-    );
-    const cardIndex = myData.lists[listIndex].cards.findIndex(
-      (el) => el.id === cardId
-    );
+    const listIndex = parseInt(myData.lists.findIndex((el) => el.id === listId));
+    const cardIndex = myData.lists[listIndex].cards.findIndex((el) => el.id === cardId);
 
-    const newCardArr = this.validateCardTitle(myData, newVal, cardId);
+    const newCardArr = validateCardTitle(myData, newVal, cardId);
 
     myData.lists[listIndex].cards[cardIndex] = newCardArr;
 
-    this.setState({ boardData: myData });
-    this.props.onBoardDataChange(myData);
+    setBoardData(myData);
+    props.onBoardDataChange(myData);
   }
 
-  handleArchiveCard(event) {
-    const myData = { ...this.state.boardData };
+  function handleArchiveCard(event) {
+    const myData = { ...boardData };
     const listId = parseInt(event.target.closest("div").dataset.grandfather);
     const cardId = parseInt(event.target.closest("div").dataset.father);
 
-    const listIndex = parseInt(
-      myData.lists.findIndex((el) => el.id === listId)
-    );
-    const cardIndex = myData.lists[listIndex].cards.findIndex(
-      (el) => el.id === cardId
-    );
+    const listIndex = parseInt(myData.lists.findIndex((el) => el.id === listId));
+    const cardIndex = myData.lists[listIndex].cards.findIndex((el) => el.id === cardId);
 
     myData.lists[listIndex].cards.splice(cardIndex, 1);
 
-    this.setState({ boardData: myData });
-    this.props.onBoardDataChange(myData);
+    setBoardData(myData);
+    props.onBoardDataChange(myData);
   }
 
-  validateCardTitle(data, cardVal, cardId) {
+  function validateCardTitle(data, cardVal, cardId) {
     if (!cardVal) {
       return;
     }
     let cardTitle = cardVal;
     // get index of list
-    const myCardId = cardId || parseInt(
-          data.lists.map((x) => x.cards.length).reduce((a, b) => a + b)
-        ) + 1;
+    const myCardId =
+      cardId || parseInt(data.lists.map((x) => x.cards.length).reduce((a, b) => a + b)) + 1;
 
     // extract hashtags
     const tags = cardTitle
@@ -191,35 +164,28 @@ class BoardTemplate extends React.Component {
     return newCardArr;
   }
 
-  render() {
-    const pageStyle =
-      "h-screen overflow-hidden select-none flex flex-col bg-[" +
-      this.state.boardData.background +
-      "]";
-    // bg-[#0079bf] bg-[#00aecc]
+  const pageStyle =
+    "h-screen overflow-hidden select-none flex flex-col bg-[" + boardData.background + "]";
+  // bg-[#0079bf] bg-[#00aecc]
 
-    return (
-      <div className={pageStyle}>
-        <BoardHeader
-          data={this.state.boardData}
-          onChangeBoardTitle={this.handleBoardTitleChange}
-        />
-        <BoardLists
-          data={this.state.boardData.lists}
-          onSubmitNewList={this.handleSubmitNewList}
-          inputAddNewList={this.state.inputAddNewList}
-          onChangeInputAddNewList={this.handleChangeInputAddNewList}
-          onSubmitNewCard={this.handleSubmitNewCard}
-          inputAddNewCard={this.state.inputAddNewCard}
-          onChangeInputAddNewCard={this.handleChangeInputAddNewCard}
-          onChangeListTitle={this.handleChangeListTitle}
-          onChangeCard={this.handleChangeCard}
-          onArchiveList={this.handleArchiveList}
-          onArchiveCard={this.handleArchiveCard}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={pageStyle}>
+      <BoardHeader data={boardData} onChangeBoardTitle={handleBoardTitleChange} />
+      <BoardLists
+        data={boardData.lists}
+        onSubmitNewList={handleSubmitNewList}
+        inputAddNewList={inputAddNewList}
+        onChangeInputAddNewList={handleChangeInputAddNewList}
+        onSubmitNewCard={handleSubmitNewCard}
+        inputAddNewCard={inputAddNewCard}
+        onChangeInputAddNewCard={handleChangeInputAddNewCard}
+        onChangeListTitle={handleChangeListTitle}
+        onChangeCard={handleChangeCard}
+        onArchiveList={handleArchiveList}
+        onArchiveCard={handleArchiveCard}
+      />
+    </div>
+  );
 }
 
 export default BoardTemplate;
