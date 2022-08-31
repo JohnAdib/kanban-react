6fdbccac97f25678a55aa591a6cd3c03c150dd1b
MrAdib <Mr.Javad.Adib@gmail.com>
2022-08-31 19:11:37 +0430

    add new story for each part of design system

---
+++ src/molecules/ClickToEdit.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import ClickToEdit from './ClickToEdit';
+
+export default {
+  title: 'Molcules/ClickToEdit',
+  component: ClickToEdit,
+};
+
+const Template = (args) => <ClickToEdit {...args}>{args.title}</ClickToEdit>;
+
+export const ClickHere = Template.bind({});
+ClickHere.args = {
+  color: "red",
+  father: 100,
+  grandfather: 200,
+  title: "Click Here",
+  value: "Click Here",
+  placeholder: "Enter some value...",
+  inputClass: "font-bold",
+};
